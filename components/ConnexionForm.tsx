5d3fd69c5e4f9a84fccdbbe2d30c8216d23f7331
AntoineRai <antoine.raimand@gmail.com>
2024-05-13 11:08:02 +0200

    Add new dependencies and update tailwind configuration

---
+++ components/ConnexionForm.tsx
@@ -0,0 +1,72 @@
+"use client";
+
+import React from "react";
+
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useForm } from "react-hook-form";
+
+import { Button } from "@/components/ui/button";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+
+const formSchema = z.object({
+  email: z.string().email({ message: "Adresse-mail invalide" }),
+  password: z.string().min(0, {
+    message: "Le mot de passe ne peut pas être vide",
+  }),
+});
+
+export function ConnexionForm() {
+  const form = useForm<z.infer<typeof formSchema>>({
+    resolver: zodResolver(formSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+
+  function onSubmit(values: z.infer<typeof formSchema>) {
+    console.log(values);
+  }
+
+  return (
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-8">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="mail" placeholder="Adresse-mail" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormControl>
+                <Input type="password" placeholder="Mot de passe" {...field} />
+              </FormControl>
+              <FormMessage />
+            </FormItem>
+          )}
+        />
+        <Button type="submit">Connexion</Button>
+      </form>
+    </Form>
+  );
+}
+
+export default ConnexionForm;
